ee99995ecf2ffb2a1a8ca51bb6c56cc1041ed83f
Jessica-Mansell <jcmansell88@gmail.com>
2023-02-02 22:40:49 -0600

    Initial Commit

---
+++ Xcode Demo/Xcode_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Xcode_DemoApp.swift
+//  Xcode Demo
+//
+//  Created by Jessica Mansell on 2/2/23.
+//
+
+import SwiftUI
+
+@main
+struct Xcode_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
